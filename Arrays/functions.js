a5849b081f135685567001bf2af5775692e32eb6
Rivojiddin Abdunazarov <68281739+din0497@users.noreply.github.com>
2022-01-03 23:16:40 +0500

    Function is explained

---
+++ Arrays/functions.js
@@ -0,0 +1,25 @@
+// 
+// function opka () {
+//     // qisish
+//     // havoni burun orqali tubelariga yuboradi
+//     // bla bla
+     
+//     return kislarod
+// }
+
+// function multiply () {
+//     let result = 2 * 2;
+    
+//     return result;
+
+// }
+
+
+// let data = '';
+
+// function getName (name) {
+//   data = name
+// }
+
+
+
